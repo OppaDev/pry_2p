e7c980090be0bf23d4cc18376f8890ceab6bd565
OppaDev <jlobando2@espe.edu.ec>
2025-07-08 11:31:32 -0500

    asignatura user

---
+++ app/Http/Controllers/AsignaturaUserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AsignaturaUser;
+use Illuminate\Http\Request;
+
+class AsignaturaUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AsignaturaUser $asignaturaUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AsignaturaUser $asignaturaUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AsignaturaUser $asignaturaUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AsignaturaUser $asignaturaUser)
+    {
+        //
+    }
+}
