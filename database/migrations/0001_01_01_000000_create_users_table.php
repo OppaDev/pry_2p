193a41557371a94d0e4fda91e315b12c2ed51e68
OppaDev <jlobando2@espe.edu.ec>
2025-07-08 11:27:25 -0500

    base

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('estado')->nullable();
+            $table->string('motivo')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
